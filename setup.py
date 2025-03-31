51ad37c899b175398200df4a1320661cd3450735
vikramjitbora <vikramjit.bora@gmail.com>
2025-03-31 10:27:37 +0530

    Complete Setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements()->List[str]:
+    """
+    Function to return requirements from requirements.txt as a list
+    """
+    requirements_lst:List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            # Read lines from file
+            lines = file.readlines()
+            # Process each line
+            for line in lines:
+                requirement = line.strip()
+                # Ignore empty lines and -e .
+                if requirement and requirement!='-e .':
+                    requirements_lst.append(requirement)
+    
+    except FileNotFoundError:
+        print('requirements.txt file not found')
+    
+    return requirements_lst
+
+setup(
+    name='Network Security',
+    version='0.0.1',
+    author='Vikramjit Bora',
+    author_email='boravikramjit@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements()
+)
